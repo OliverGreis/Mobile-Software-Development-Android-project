040843f7de7c94b80551a1cb2858f57196a70a2d
sofie <sofim23@student.sdu.dk>
2025-11-23 17:23:39 +0100

    some controller logic (not finished nor correct)

--- app/src/main/java/com/example/myapplication/Add_payment.kt
+++ app/src/main/java/com/example/myapplication/Add_payment.kt
@@ -1,20 +1,41 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Switch
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Add_payment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+class Add_payment : AppCompatActivity()
+{
+  lateinit var Card: CardView
+  lateinit var account: CardView
+  lateinit var createButton: Button
+  lateinit var backButton: Button
+
+  // card view
+  lateinit var cardNumberInput: EditText
+  lateinit var expiryDateInput: EditText
+  lateinit var CVCInput: EditText
+
+  // account view
+  lateinit var accountNumberInput: EditText
+  lateinit var accountNameInput: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_add_payment)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
+
+
+
+
