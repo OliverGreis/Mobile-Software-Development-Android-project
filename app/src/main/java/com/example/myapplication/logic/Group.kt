21ace96d76c0df7767b385e1f2e162c879cabc5c
matkr20 <matkr20@student.sdu.dk>
2025-11-24 08:26:49 +0100

    Added comment documentation

--- app/src/main/java/com/example/myapplication/logic/Group.kt
+++ app/src/main/java/com/example/myapplication/logic/Group.kt
@@ -11,26 +11,31 @@ class Group() {
         return response
     }
 
+    //"/api/groups/member/{id}"
     suspend fun getGroupsMember(urlInput:String): HttpResponse {
         val response: HttpResponse = Network.sendGetRequest("http://localhost:8080/api/groups/member/$urlInput")
         return response
     }
 
+    //"/api/group/{id}"
     suspend fun GetGroup(urlInput:String): HttpResponse {
         val response: HttpResponse = Network.sendGetRequest("http://localhost:8080/api/group/$urlInput")
         return response
     }
 
+    //"/api/addmember/{id}/{groupID}"
     suspend fun addGroupMember(urlInput:String): HttpResponse {
         val response: HttpResponse = Network.sendPutRequest("http://localhost:8080/api/addmember/$urlInput")
         return response
     }
 
+    //"/api/addtransaction/{id}/{groupID}"
     suspend fun addTransaction(urlInput:String): HttpResponse {
         val response: HttpResponse = Network.sendPutRequest("http://localhost:8080/api/addtransaction/$urlInput")
         return response
     }
 
+    //"api/creategroup/{name}"
     suspend fun createGroup(urlInput:String): HttpResponse {
         val response: HttpResponse = Network.sendPostRequest("http://localhost:8080/api/creategroup/$urlInput")
         return response
