a3ee9eab83fec62d27475875520316a24fe974cd
Nicklas <nidam23@student.sdu.dk>
2026-01-02 10:17:53 +0100

    Merge some files

--- app/src/main/java/com/example/myapplication/model/ResetRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.model
-
-data class ResetRequest(
-    val email: String,
-    val newPassword: String
-)
--- app/src/main/java/com/example/myapplication/model/UserCreateRequest.kt
+++ app/src/main/java/com/example/myapplication/model/UserCreateRequest.kt
@@ -6,3 +6,13 @@ data class RegisterRequest(
     val password: String,
     val number: String
 )
+
+data class LoginRequest(
+    val email: String,
+    val password: String
+)
+
+data class ResetRequest(
+    val email: String,
+    val newPassword: String
+)
